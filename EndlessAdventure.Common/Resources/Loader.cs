7c74a4839bc2d649246f6fd43700de41d26b40fc
Albert Tang <AlbertTangCS@gmail.com>
2017-10-16 20:06:33 -0700

    fixed build lol

--- EndlessAdventure.Common/Resources/Loader.cs
+++ EndlessAdventure.Common/Resources/Loader.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using EndlessAdventure.Common.Characters;
 
-namespace EndlessAdventure.Common.Resources
-{
-	public class Loader
-	{
-		public static List<EnemyData> GetEnemyData(string world) {
-			return new List<EnemyData>(EnemyDatabase.Enemies.Where(data => data.World == world));
+namespace EndlessAdventure.Common.Resources {
+
+	public class Loader {
+
+		public static List<CombatantData> GetEnemyData(string world) {
+			return new List<CombatantData>(EnemyDatabase.Enemies.Where(data => data.World == world));
 		}
 	}
+
 }
--- EndlessAdventure.Common/World.cs
+++ EndlessAdventure.Common/World.cs
@@ -2,24 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using EndlessAdventure.Common.Battle;
+using EndlessAdventure.Common.Characters;
 using EndlessAdventure.Common.Resources;
 
 namespace EndlessAdventure.Common {
 	public class World {
 
 		public string Name { get; private set; }
-		private Dictionary<int, EnemyData> _enemyData;
+		private Dictionary<int, CombatantData> _enemyData;
 		private List<int> _weights;
 		private int _totalWeight;
 
-		public World(string name, List<EnemyData> enemyData) {
+		public World(string name, List<CombatantData> enemyData) {
 			if (name == null || enemyData == null) throw new ArgumentException();
 
 			Name = name;
-			_enemyData = new Dictionary<int, EnemyData>();
+			_enemyData = new Dictionary<int, CombatantData>();
 			_weights = new List<int>();
 
-			foreach (EnemyData data in enemyData) {
+			foreach (CombatantData data in enemyData) {
 				_totalWeight += data.Weight;
 				_enemyData.Add(_totalWeight, data);
 				_weights.Add(_totalWeight);
@@ -33,7 +34,7 @@ namespace EndlessAdventure.Common {
 
 			// get the key that is the random value rounded up
 			int key = _weights.FirstOrDefault( x => x >= result);
-			_enemyData.TryGetValue(key, out EnemyData data);
+			_enemyData.TryGetValue(key, out CombatantData data);
 
 			return CombatantFactory.CreateCombatant(data.Name,
 																							attack: data.Attack,
